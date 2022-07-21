b4bbf20b9f547aafcfcbedcc0579b2e8a29a67f8
Loucong <loucongxd@gmail.com>
2022-07-21 10:03:36 -0300

    ArrayStrintTest Added

---
+++ src/main/kotlin/one/digitalinnovation/collections/ArrayStringTest.kt
@@ -0,0 +1,21 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val nomes = Array(3) {""}
+    nomes[0] = "Mario"
+    nomes[1] = "Zaza"
+    nomes[2] = "Jose"
+
+    for (nome in nomes) {
+        println(nome)
+    }
+
+space()
+    nomes.sort()
+    nomes.forEach { println(it) }
+
+space()
+    val nomes2 = arrayOf("Mario", "Zaza", "Jose")
+    nomes2.sort()
+    nomes2.forEach { println(it) }
+}
